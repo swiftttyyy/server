1d97e80cb1e68b4a25ae646997dea69492f60206
swift <donaldmiller5409@gmail.com>
2024-06-19 15:00:56 +0100

    second commit

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.post("/sender", async(req,res) => {
     });
 })
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
